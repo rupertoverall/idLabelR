fd3787c76cf66059c5e574071962de488d08d6aa
rupertoverall <35666678+rupertoverall@users.noreply.github.com>
2021-11-10 20:50:28 +0100

    Added Herma_4333 paper label template.

--- R/get_layout.R
+++ R/get_layout.R
@@ -39,6 +39,26 @@ get_layout = function(label.type){
 		)
 	}
 
+	if(label.type == "Herma_4333_A4"){
+		layout = list(
+			# Positions on the page of the label centres.
+			label.x = c(20.5, 48.5, 76.5, 104.5, 132.5, 160.5, 188.5),
+			label.y = c(18, 28, 38, 48, 58, 68, 78, 88, 98, 108, 118, 128, 138, 148, 158, 168, 178, 188, 198, 208, 218, 228, 238, 248, 258, 268, 278),
+			page.width = 210,
+			page.height = 297,
+			units = "mm",
+			text.font = 2, # Bold.
+			text.size = 0.8, # 80% default size.
+			sublabel.font = 1, # Normal.
+			sublabel.size = 0.5, # 50% default size.
+			sublabel.offset = 2, # Distance below ID text.
+			# Optional date-of-printing timestamp position.
+			timestamp.x = 210 - 15,
+			timestamp.y = 17,
+			timestamp.size = 0.75
+		)
+	}
+
 	if(is.null(layout)) warning("There is no available label configuration for '", label.type, "'.")
 	return(layout)
 }
